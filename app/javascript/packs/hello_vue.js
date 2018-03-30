9231e571f2a53085b23227e90fc29b866c65f619
DmitryR <paukov2967@gmail.com>
2018-03-30 19:13:37 +0300

    liquor tree test

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -35,19 +35,73 @@
 
  import Vue from 'vue/dist/vue.esm'
  //import App from '../app.vue'
+ import VueTree from 'liquor-tree/dist/liquor-tree.esm'
+
+Vue.use(VueTree)
 
  document.addEventListener('DOMContentLoaded', () => {
-   const app = new Vue({
+
+   const app =     new Vue({
        el: '#app',
-       data: {
-           title: 'Hello World!'
-       },
-       methods: {
-           changeTitle: function(event) {
-               this.title = event.target.value;
+       data: function() {
+           return {
+               treeData: getTreeData(),
+               treeOptions: {
+                   checkbox: true,
+                   propertyNames: {
+                       text: 'MY_TEXT',
+                       children: 'KIDS'
+                   }
+               }
            }
        }
    })
+
+
+     function getTreeData() {
+         return [
+             {
+                 MY_TEXT: 'JS: The Right Way',
+                 state: {expanded: true},
+                 KIDS: [
+                     {MY_TEXT: 'Getting Started', state: {checked: true}},
+                     {MY_TEXT: 'JavaScript Code Style', state: {selected: true}},
+                     {
+                         MY_TEXT: 'The Good Parts', KIDS: [
+                             {MY_TEXT: 'OBJECT ORIENTED', state: {checked: true}},
+                             {MY_TEXT: 'ANONYMOUS FUNCTIONS', state: {checked: true}},
+                             {MY_TEXT: 'FUNCTIONS AS FIRST-CLASS OBJECTS', state: {checked: true}},
+                             {MY_TEXT: 'LOOSE TYPING', state: {checked: true}}
+                         ]
+                     },
+                     {
+                         MY_TEXT: 'Patterns', KIDS: [
+                             {
+                                 MY_TEXT: 'DESIGN PATTERNS', state: {expanded: true}, KIDS: [
+                                     {
+                                         MY_TEXT: 'Creational Design Patterns', KIDS: [
+                                             {MY_TEXT: 'Factory'},
+                                             {MY_TEXT: 'Prototype'},
+                                             {MY_TEXT: 'Mixin'},
+                                             {MY_TEXT: 'Singleton'}
+                                         ]
+                                     },
+                                     {MY_TEXT: 'Structural Design Patterns'}
+                                 ]
+                             },
+                             {
+                                 MY_TEXT: 'MV* PATTERNS', cildren: [
+                                     {MY_TEXT: 'MVC Pattern'},
+                                     {MY_TEXT: 'MVP Pattern'},
+                                     {MY_TEXT: 'MVVM Pattern'}
+                                 ]
+                             }
+                         ]
+                     }
+                 ]
+             }
+         ]
+     }
  })
 //
 //
